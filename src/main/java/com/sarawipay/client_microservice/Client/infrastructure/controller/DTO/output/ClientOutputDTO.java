2c16997c2dc49cf6b8618c19ebc970358e729c64
sara-lopez <sara.lopez@petroprix.com>
2025-04-03 12:39:20 +0200

    fix: [OUTPUT]: Formatear nombres y apellidos como nombres propios

--- src/main/java/com/sarawipay/client_microservice/Client/infrastructure/controller/DTO/output/ClientOutputDTO.java
+++ src/main/java/com/sarawipay/client_microservice/Client/infrastructure/controller/DTO/output/ClientOutputDTO.java
@@ -16,4 +16,22 @@ public class ClientOutputDTO {
     private String surname;
     private String phone;
     private String email;
+
+
+    // Necesitamos este setter para poder convertir el nombre a minúsculas
+    public void setName(String name) {
+        StringBuilder sb = new StringBuilder(name.length());
+        sb.append(Character.toUpperCase(name.charAt(0)));
+        sb.append(name.substring(1).toLowerCase());
+
+        this.name = sb.toString();
+    }
+
+    public void setSurname(String surname){
+        StringBuilder sb = new StringBuilder(surname.length());
+        sb.append(Character.toUpperCase(surname.charAt(0)));
+        sb.append(surname.substring(1).toLowerCase());
+
+        this.surname = sb.toString();
+    }
 }
